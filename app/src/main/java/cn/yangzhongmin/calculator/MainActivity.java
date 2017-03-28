bf858ce0aad232ab74c0997c4cbd458a363a6b76
yangzhongmin <yangzhongmin19@126.com>
2017-03-29 00:19:32 +0800

    新建一个简易计算器程序，添加了两个activity：主界面和历史记录界面。 并完成主界面的布局

---
+++ app/src/main/java/cn/yangzhongmin/calculator/MainActivity.java
@@ -0,0 +1,21 @@
+package cn.yangzhongmin.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by yzm on 2017/3/28.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.layout_main);
+
+
+    }
+}
